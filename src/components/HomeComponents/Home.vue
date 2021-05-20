9aec51bc6388d2bc25b3153001f0098e6f17764e
ALEX <wangbinim@outlook.com>
2021-05-20 12:49:09 +0800

    Hidden net worth, net worth has error

--- src/components/HomeComponents/Home.vue
+++ src/components/HomeComponents/Home.vue
@@ -2,7 +2,7 @@
   <el-row>
     <el-col :span="24">
       <InvestmentPieChart :item="InvestmentValue" />
-      <InvestmentHistogram :item="InvestmentValue" />
+      <!-- <InvestmentHistogram :item="InvestmentValue" /> -->
     </el-col>
     <el-col :span="24">
       <BillLineChart
