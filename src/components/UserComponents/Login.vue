acd9af438daa36666932ab53c83efcd99c064b1f
ALEX <wangbinim@outlook.com>
2021-08-26 08:44:51 +0800

    fix error

--- src/components/UserComponents/Login.vue
+++ src/components/UserComponents/Login.vue
@@ -18,21 +18,48 @@
     <el-col :span="4">
       <el-checkbox v-model="remember">记住我</el-checkbox>
     </el-col>
+  <el-row>
+    <el-col :span="4">
+      <el-button type="primary" @loading="loading" @click="login()">登陆</el-button>
+    </el-col>
   </el-row>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import router from "../../router";
+import api from "../../api/index";
 
 export default defineComponent({
   data() {
     return {
       userName: "",
       password: "",
+      loading: false,
       remember: false,
     };
   },
-  methods: {},
+  methods: {
+    login() {
+      this.loading = true
+      var value = {
+        userName: this.userName,
+        password: this.password,
+      }
+      this.axios
+        .post(api.validateToken, value)
+        .then((response) => {
+          if (response.data.successful == true) {           
+            router.push({ path: "/Filter/Home", params: { id: response.data.data } });
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+          this.loading = false
+        });
+      return;
+    }
+  },
 });
 </script>
 
