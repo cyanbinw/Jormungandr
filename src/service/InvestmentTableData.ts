5f145d30dcd04793c55396bf7cc31c5a2e4cc643
ALEX <wangbinim@outlook.com>
2021-10-11 08:48:19 +0800

    add ServiceCharge to Investment

--- src/service/InvestmentTableData.ts
+++ src/service/InvestmentTableData.ts
@@ -11,6 +11,7 @@ export default class InvestmentTableData {
     public Account: number = 0;
     public Share: number = 0;
     public NetWorth: number = 0;
+    public ServiceCharge: number = 0;
     public Date: any = 0;
     public ActivityStatus: number = 0;
     public ActivityName: string = ''
@@ -25,6 +26,7 @@ export default class InvestmentTableData {
         this.Account = data.account;
         this.Share = data.share;
         this.NetWorth = data.netWorth;
+        this.ServiceCharge = data.serviceCharge;
         this.Date = moment((data.date as Date)).format("yyyy-MM-DD");
         this.ActivityStatus = data.activity;
         this.ActivityName = data.activityName;
@@ -40,6 +42,7 @@ export default class InvestmentTableData {
         this.Account = data.account;
         this.Share = data.share;
         this.NetWorth = data.netWorth;
+        this.ServiceCharge = data.serviceCharge;
         this.Date = moment((data.date as Date)).format("yyyy-MM-DD");
         this.ActivityStatus = data.activityStatus;
         this.ActivityName = data.activityName;
@@ -54,6 +57,7 @@ export class InvestmentData {
     public account!: number
     public share!: number
     public netWorth!: number
+    public serviceCharge!: number
     public type!: number
     public activity!: number
     public date!: Date
@@ -69,6 +73,7 @@ export class InvestmentData {
         this.account = Number(data.account)
         this.share = Number(data.share)
         this.netWorth = Number(data.netWorth)
+        this.serviceCharge = Number(data.serviceCharge)
         this.type = Number(data.type)
         this.activity = Number(data.activity)
         this.date = new Date(moment((data.date as Date)).format("yyyy-MM-DD"))
@@ -82,6 +87,7 @@ export class InvestmentData {
         this.account = Number(data.Account)
         this.share = Number(data.Share)
         this.netWorth = Number(data.NetWorth)
+        this.serviceCharge = Number(data.ServiceCharge)
         this.type = Number(data.TypeID)
         this.activity = Number(data.ActivityStatus)
         this.date = new Date(moment((data.Date as Date)).format("yyyy-MM-DD"))
