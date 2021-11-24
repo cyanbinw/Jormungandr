1e6d69812c8f8e5688fc39cfa3cb517349dfa2ee
ALEX <wangbinim@outlook.com>
2021-11-24 12:14:17 +0800

    set UI

--- src/components/InvestmentComponents/InvestmentReportForm.vue
+++ src/components/InvestmentComponents/InvestmentReportForm.vue
@@ -10,29 +10,92 @@
       height: 75%;
     "
   >
-    <el-descriptions title="Customized style list" :column="3" border>
-      <el-descriptions-item
-        label="Username"
-        label-align="right"
-        align="center"
-        label-class-name="my-label"
-        class-name="my-content"
-        width="150px"
-        >kooriookami</el-descriptions-item
-      >
-      <el-descriptions-item label="Telephone" label-align="right" align="center"
-        >18100000000</el-descriptions-item
-      >
-      <el-descriptions-item label="Place" label-align="right" align="center"
-        >Suzhou</el-descriptions-item
-      >
-      <el-descriptions-item label="Remarks" label-align="right" align="center">
-        <el-tag size="small">School</el-tag>
+    <el-descriptions title="投资总览" :column="4" border>
+      <el-descriptions-item label="持有" label-align="center" align="center">
+        6个
       </el-descriptions-item>
-      <el-descriptions-item label="Address" label-align="right" align="center"
-        >No.1188, Wuzhong Avenue, Wuzhong District, Suzhou, Jiangsu
-        Province</el-descriptions-item
+      <el-descriptions-item
+        label="总投资"
+        label-align="center"
+        align="center"
+        :span="2"
+        >15000</el-descriptions-item
       >
+      <el-descriptions-item
+        label="收回"
+        label-align="center"
+        align="center"
+        :span="2"
+      >
+        4000
+      </el-descriptions-item>
+      <el-descriptions-item
+        label="手续费"
+        label-align="center"
+        align="center"
+        :span="2"
+      >
+        20
+      </el-descriptions-item>
+      <el-descriptions-item
+        label="盈余"
+        label-align="center"
+        align="center"
+        :span="2"
+        >500</el-descriptions-item
+      >
+      <el-descriptions-item
+        label="仓位"
+        label-align="center"
+        align="center"
+        :span="4"
+        >特变电工 (20%) 生物医药 (10%) 特变电工 (20%) 生物医药 (10%) 特变电工
+        (20%) 生物医药 (10%)
+      </el-descriptions-item>
+      <el-descriptions-item label="清空" label-align="center" align="center">
+        3个
+      </el-descriptions-item>
+      <el-descriptions-item
+        label="总投资"
+        label-align="center"
+        align="center"
+        :span="2"
+        >15000</el-descriptions-item
+      >
+      <el-descriptions-item label="收益" label-align="center" align="center"
+        >500</el-descriptions-item
+      >
+      <el-descriptions-item label="手续费" label-align="center" align="center">
+        20
+      </el-descriptions-item>
+      <el-descriptions-item
+        label="名称"
+        label-align="center"
+        align="center"
+        :span="4"
+        >特变电工 (20%) 生物医药 (10%) 特变电工 (20%) 生物医药 (10%) 特变电工
+        (20%) 生物医药 (10%)
+      </el-descriptions-item>
+      <el-descriptions-item label="总计" label-align="center" align="center">
+        9个
+      </el-descriptions-item>
+      <el-descriptions-item
+        label="总投资"
+        label-align="center"
+        align="center"
+        :span="2"
+        >15000</el-descriptions-item
+      >
+      <el-descriptions-item label="收益" label-align="center" align="center"
+        >500</el-descriptions-item
+      >
+      <el-descriptions-item label="手续费" label-align="center" align="center">
+        20
+      </el-descriptions-item>
+      <el-descriptions-item label="收益展示" label-align="center" align="center"
+        >特变电工 (20%) 生物医药 (10%) 特变电工 (20%) 生物医药 (10%) 特变电工
+        (20%) 生物医药 (10%)
+      </el-descriptions-item>
     </el-descriptions>
   </div>
 </template>
